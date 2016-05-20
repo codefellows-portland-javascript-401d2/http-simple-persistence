c3d530995498ee3b2c6ad95710a941b26a3a609c
DonChatelain <djchatelain@gmail.com>
2016-05-19 17:46:21 -0700

    improving tests

--- test/server.test.js
+++ test/server.test.js
@@ -7,38 +7,61 @@ const server = require('../src/server');
 chai.use(chaiHttp);
 
 describe('running http server', () => {
-  
-  const request = chai.request(server);
-  
-  // // WIP 
-  // it(' responds with list on GET /dogs', (done) => { 
-  //   assert.equal('true', false);
-  //   done();
-  // });
 
-  // it('puts', (done) => {
-  //   assert.equal('true', false);
-  //   done();
-  // });
-  
-  it('END 2 End post request', (done) => {
-    var post = '{"breed": "doberman"}';
+  var request = chai.request(server);
+  var post = '{"breed": "doberman"}';
+   
+  it('posts', (done) => {
     request
         .post('/')
         .send(post)
-        .then(res => {
-          return res.id;
-        })
-        .then( id => {
-          return request.get(`/dogs/${id}`);
-        })
-        .then( res => {
-          assert.equal(res.statusCode, 200);
+        .end((err, response) => {
+          if (err) console.log(err);
+          assert.equal(response.statusCode, 200);
           done();
-        })
-        .catch( done );
-        
+        });
   });
+  
+  it('gets posted resource', (done) => {
+    request
+        .get('/dogs/doberman.json')
+        .end((err, response) => {
+          assert.equal(response.text, post);
+          done();
+        });
+  });
+  
+  it('deletes posted resource', (done) => {
+    request
+        .del('/dogs/doberman.json')
+        .end((err, response) => {
+          if (err) throw err;
+          var resArray = response.text.split(' ');
+          assert.equal('doberman.json', resArray[0]);
+          done();
+        });
+  });
+  
+  // it('puts second resource', (done) => {
+  //   var newPost = 
+  //   request
+  //       .put('/dogs/')
+  //       .send(post)
+  //       .end((err, response) => {
+  //         if (err) console.log(err);
+  //         assert.equal(response.statusCode, 200);
+  //         done();
+  //       });
+  // });
+  
+  // it ('lists all file names in api at path /dogs', done => {
+  //   request
+  //       .get('/dogs')
+  //       .end((err, response) => {
+  //         if (err) throw err;
+  //         assert.equal(1, 1); //  ! ! ! ! !
+  //       });
+  // });
   
   it('routes to 404 page when path is not recognized', (done) => {
     chai.request(server)
